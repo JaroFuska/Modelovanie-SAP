e1810bfcb26c99b1fa351f27ca70e35983f05957
JaroFuska <jarofuska@gmail.com>
2017-12-05 08:46:55 +0100

    Initial commit

---
+++ src/sample/Box.java
@@ -0,0 +1,53 @@
+package sample;
+
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+import java.util.Random;
+
+
+/**
+ * Created by Jaro on 2.12.2017.
+ */
+public class Box extends Rectangle {
+    private double dx;
+    private double dy;
+    private double w;
+    private double h;
+    private Random rnd;
+
+    public Box(double x, double y, double w, double h) {
+        this.setPos(x, y);
+        this.w = w;
+        this.h = h;
+        this.setStroke(Color.BLACK);
+        this.setHeight((double)new Random().nextInt(25)+10);
+        this.setWidth((double)new Random().nextInt(25)+10);
+        this.rnd = new Random();
+        do {
+            this.dx = (rnd.nextInt(21) - 10) / 10.0;
+            this.dy = (rnd.nextInt(21) - 10) / 10.0;
+        } while ((Math.abs(dx) < 0.2 || Math.abs(dy) < 0.2));
+        this.toFront();
+    }
+
+    public void setPos(double x, double y) {
+        setX(x);
+        setY(y);
+    }
+
+    public void move(){
+        double x = getX()+dx;
+        double y = getY()+dy;
+        if (x <= 0 || x >= w-getWidth()) {
+            dx *= -1;
+            x = getX()+dx;
+        }
+        if (y <= 0 || y >= h-getHeight()) {
+            dy *= -1;
+            y = getY()+dy;
+        }
+        setPos(x, y);
+    }
+
+}
---
+++ src/sample/Main.java
@@ -0,0 +1,84 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Main extends Application implements Runnable {
+    Pane pg;
+    double panelWidth;
+    int n = 1000;
+    Random rnd = new Random();
+    int w = 800, h = 600;
+    ArrayList<Box> boxes;
+    Thread thread;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        panelWidth = 120;
+        primaryStage.setTitle("Sweep And Prune");
+        BorderPane borderPane = new BorderPane();
+        Scene root = new Scene(borderPane, w, h);
+        pg = new Pane();
+        pg.setMinHeight(root.getHeight());
+        pg.setMinWidth(root.getWidth()-panelWidth);
+        VBox vb = new VBox();
+        //TODO add some menu to the vb
+        //TODO set width of menu components to panelWidth
+        borderPane.setLeft(vb);
+
+
+        boxes = new ArrayList();
+        for (int i = 0; i < n; i++) {
+            boxes.add(new Box((double)rnd.nextInt(w-40), (double)rnd.nextInt(h-40), w, h));
+        }
+        pg.getChildren().addAll(boxes);
+
+
+
+
+        thread = new Thread(this);
+        thread.start();
+        borderPane.setCenter(pg);
+        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent t) {
+                Platform.exit();
+                System.exit(0);
+            }
+        });
+        primaryStage.setScene(root);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void run() {
+        //TODO move boxes
+        while(true) {
+            try {
+                thread.sleep(20);
+            } catch (Exception e) {
+                // TODO: handle exception
+            }
+            for (Box b : boxes
+                 ) {
+                b.move();
+
+            }
+        }
+    }
+}
